75034378e960dd2a21cc1d8f5d70ae871ae11238
HimadriSingh22 <rajput.2220.himadri@gmail.com>
2019-02-18 23:21:12 +0530

    first Stage

---
+++ src/main/java/imagePackage/ImageHosterApp.java
@@ -0,0 +1,12 @@
+package imagePackage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageHosterApp {
+    public static void main(String[] args){
+        SpringApplication.run(ImageHosterApp.class,args);
+    }
+
+}
